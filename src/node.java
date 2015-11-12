566a1230f4e5961515b82692b451130abd2b3f4f
kem100 <kemosito@gmail.com>
2015-11-11 18:42:27 -0800

    Update node
    node using linked list

--- src/node.java
+++ src/node.java
@@ -1,4 +1,37 @@
 
 public class node {
+  
+  private String name;
+	private Neighbour adjList;
+	
+	//constructor for the Node(location)
+	//takes in a String representing the locations name and a List of Adjacent locations
+	public node(String nme, Neighbour list){
+		
+		setName(nme);
+		setAdjList(list);
+	}//end of constructor
+	
+	//sets the Name of the location
+	public void setName(String Name){
+		name = Name;
+	}//end of method setName
+	
+	//returns the Name of the location
+	public String getName(){
+		return name;
+	}//end of method getName
+	
+	//sets the list of the location's Neighbours.
+	public void setAdjList(Neighbour list){
+		adjList = list;
+	}//end of method setAdjList
+	
+	//returns the list of adjacent points
+	public Neighbour getAdjList(){
+		return adjList;
+	}//end of method getAdjList
+	
+	
 
-}
+}//end class node
